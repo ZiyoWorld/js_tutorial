ec6f30005d4a92298234abc5b53b23081cb957c9
Mirziyod_ms <sunatillayevmirziyod363@gmail.com>
2023-01-03 01:10:18 +0300

    numbers

---
+++ 8-dars/index.js
@@ -0,0 +1,79 @@
+//Numbers
+
+//console.log(100_000_000 + 1);
+//console.log(12.2215151);
+//console.log(1e6);
+
+//console.log(1e-6 + 5);
+
+// console.log(1e500);
+// console.log(0.2 + 0.2 === 0.4);
+// console.log(0.2 + 0.1 === 0.3);
+
+
+// console.log(Number(0.1 + 0.2).toFixed(5));
+
+// let num = "0.1 + 0.2*1";
+// console.log(parseInt(num));
+// console.log(parseFloat(num));
+// console.log(eval(num));
+
+// let num = 5;
+// console.log(num.toString(16));
+// console.log(parseInt("5", 16));
+
+
+// console.log(isNaN(+"1000"));
+
+// let res = Object.is("web", "web");
+// console.log(res);
+
+// let num = 5.15151511;
+// let num1 = 0.001256;
+// console.log(num.toPrecision(3));
+// console.log(num1.toPrecision(3));
+
+// console.log(9_999_999_999_999_999); 
+   //? // Math
+
+// console.log(Math.PI);
+//  console.log(Math.abs(-10)); // sonning moduli
+//  console.log(Math.round(10.6)); // yaxlitlash
+
+//  console.log(Math.floor(-10.999)); // pastga qarab yaxslitlaydi.
+//  console.log(Math.ceil(10.001)); // tepaga qarab yaxlitlaydi.
+//  console.log(Math.trunc(-10.999)); // . udan bu yog'ini o'chirib tashlaydi.
+
+// 10
+// 100
+// 1000
+// 10000
+// console.log(parseInt(Math.random() * 100));
+
+// console.log(Math.min(1, 5, 8, 9));
+// console.log(Math.max(1, 5, 8, 9));
+
+// console.log(Math.pow(6, 4));
+// console.log(6 ** 4);
+
+// console.log(Math.sqrt(27));
+
+// console.log(Math.sign(-0));
+// console.log(Math.sign(0));
+// console.log(Math.sign(12445));
+// console.log(Math.sign(-1));
+
+let i = 0;
+function myNumber (param) {
+    let num = Number(param.toString(2));
+    console.log(num);
+    
+    // while(num){
+    //     i += num.toString().includes("0");
+    //     i++;
+    //     console.log(i);
+    // } 
+    
+
+}
+// myNumber(50);
